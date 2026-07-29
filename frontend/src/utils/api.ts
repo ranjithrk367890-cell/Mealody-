75ad60acdef274cfc68ad853017194feab2e8497
ranjith kumar <ranjithrk37890@gmail.com>
2026-07-29 14:36:40 +0530

    new frontend and backend links are added

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://mealody.onrender.com';
 
 export const api = axios.create({
   baseURL: BASE_URL,
